5329b5b19c2c6ed1d28a027643a0ba0343eb598c
Umidjon Mamadjanov <umidjonmamadjanov217@gmail.com>
2026-09-25 00:28:04 +0500

    Delete internal directory

--- internal/core/protocol/header.go
+++
@@ -1,7 +0,0 @@
-package protocol
-
-type Header struct {
-	Version uint8
-	Type    PacketType
-	Length  uint32
-}
--- internal/core/protocol/version.go
+++
@@ -1,7 +0,0 @@
-package protocol
-
-const (
-	ProtocolVersion uint8 = 1
-	ClientVersion         = "0.1.0"
-	ServerVersion         = "0.1.0"
-)
